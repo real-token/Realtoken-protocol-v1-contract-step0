fbfc16f76ab49a36ef10037ca15cbc539258c82f
Bastien Silhol <bastien.silhol@epitech.eu>
2022-01-12 07:35:41 +0100

    deployProxy modification

--- scripts/ethers/deployProxy.ts
+++ scripts/ethers/deployProxy.ts
@@ -39,7 +39,7 @@ async function main() {
   );
   await minuteSleep();
 
-  if (network.name === "poa") {
+  if (network.name === "poa" || network.name === "xdai") {
     try {
       await run("verify:verify", {
         address: instance.address,
